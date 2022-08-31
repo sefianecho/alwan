e82b8c782f0f9193487f89737874c9e0c3959d50
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-08-31 23:56:39 +0100

    drag and move marker in the palette

--- src/js/components/Palette.js
+++ src/js/components/Palette.js
@@ -1,13 +1,100 @@
+import { MOUSE_DOWN, MOUSE_MOVE, MOUSE_UP, ROOT, TOUCH_CANCEL, TOUCH_END, TOUCH_MOVE, TOUCH_START } from "../constants";
 import { createElement, getBounds } from "../utils/dom"
 import { Marker } from "./Marker";
 
 export const Palette = (parent, talwin) => {
 
     const el = createElement('', 'talwin__palette', parent, { tabindex: '0' });
-
+    const overlay = createElement('', 'tw-overlay', parent);
+    const { style } = overlay;
     const marker = Marker(el);
+    const { on } = talwin._e;
 
-    const { WIDTH, HEIGHT } = getBounds(el);
+    const { width: WIDTH, height: HEIGHT } = getBounds(el);
+
+
+    let bounds;
+
+    let isDragging = false;
+
+
+    /**
+     * Marker start moving.
+     *
+     * @param {Event} e - Mousedown or Touchstart events.
+     * @returns {void}
+     */
+    const dragStart = e => {
+        if (e.touches && e.touches.length > 1) {
+			return;
+		}
+
+        bounds = getBounds(el);
+        moveAndUpdateColor(e);
+
+        style.display = 'block';
+        isDragging = true;
+    }
+
+    /**
+     * Moves the marker.
+     *
+     * @param {Event} e - Mousemove or Touchmove event.
+     * @returns {void}
+     */
+    const dragMove = e => {
+        if (!isDragging || (e.touches && e.touches.length > 1)) {
+			return;
+		}
+        moveAndUpdateColor(e);
+    }
+
+
+    /**
+     * Marker stop moving.
+     *
+     * @param {Event} e - Mouseup or Touchend or touchcancel events.
+     */
+    const dragEnd = e => {
+        style.display = '';
+        isDragging = false;
+    }
+
+
+    /**
+     * Moves Marker and Updates color.
+     *
+     * @param {Event} e - Drag start or drag move events.
+     */
+    const moveAndUpdateColor = e => {
+        let { top, left } = bounds;
+        let x, y;
+        let touches = e.touches;
+
+        e.preventDefault();
+
+        if (touches) {
+            e = touches[0];
+        }
+
+        // Calculate the local coordinates,
+        // local to the palette.
+        x = e.clientX - left;
+        y = e.clientY - top;
+
+        // Make sure x and y don't go out of bounds.
+		x = x < 0 ? 0 : x > WIDTH ? WIDTH : x;
+		y = y < 0 ? 0 : y > HEIGHT ? HEIGHT : y;
+
+        marker.moveTo(x, y);
+    }
+
+    /**
+     * Bind events.
+     */
+    on(el, [MOUSE_DOWN, TOUCH_START], dragStart);
+    on(ROOT, [MOUSE_MOVE, TOUCH_MOVE], dragMove, { passive: false });
+    on(ROOT, [MOUSE_UP, TOUCH_END, TOUCH_CANCEL], dragEnd);
 
     return {
         el,
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,10 @@
 export const ROOT = document;
 export const BODY = ROOT.body;
+
+export const MOUSE_DOWN = 'mousedown';
+export const MOUSE_MOVE = 'mousemove';
+export const MOUSE_UP = 'mouseup';
+export const TOUCH_START = 'touchstart';
+export const TOUCH_MOVE = 'touchmove';
+export const TOUCH_END = 'touchend';
+export const TOUCH_CANCEL = 'touchcancel';
