687fd52a2c8c82c753e9eca13700dda16ecad2ae
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-08-31 18:11:34 +0100

    add a function to check if values are set.

--- src/js/utils/util.js
+++ src/js/utils/util.js
@@ -5,3 +5,11 @@
  * @returns {Boolean}
  */
 export const isString = value => typeof value === 'string';
+
+/**
+ * Checks if a value is not undefined or null.
+ *
+ * @param {Any} value - Value.
+ * @returns {Boolean}
+ */
+export const isSet = value => value != null;
