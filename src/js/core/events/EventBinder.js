943a039ed6518e52b4a37df3595730a84c44535e
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-09-02 23:25:55 +0100

    refactor: each component has its own listeners.

--- src/js/core/events/EventBinder.js
+++ src/js/core/events/EventBinder.js
@@ -1,72 +1,57 @@
 import { isString } from "../../utils/util";
 
 /**
- * Bind/Unbind event listeners.
+ * Adds/Removes one or more event listeners to/from an element.
  *
- * @returns {Object}
+ * @param {targetElement} targetElement - Event target element.
+ * @param {Array} eventData - Event data (type, handler, options).
+ * @param {String} method - Add or Remove event listener method.
  */
-export const EventBinder = () => {
-    /**
-     * Events map to store events.
-     * 
-     * An event is stored as an array [events, handler, options],
-     * EventMap takes an element as a key and array of events as a value.
-     */
-    const eventMap = new Map();
+const binder = (eventData, unbind) => {
 
-    /**
-     * Adds/Removes one or more event listeners to/from an element.
-     *
-     * @param {targetElement} targetElement - Event target element.
-     * @param {Array} eventData - Event data (type, handler, options).
-     * @param {String} method - Add or Remove event listener method.
-     */
-    const binder = (targetElement, eventData, method) => {
+    let method = (unbind ? 'remove': 'add') + 'EventListener';
+    let [targetElement, events, handler, options] = eventData;
 
-        let [events, handler, options] = eventData;
-
-        // If its a single event then put it inside an array.
-        if (isString(events)) {
-            events = [events];
-        }
-
-        events.forEach(event => {
-            targetElement[method](event, handler, options);
-        });
+    // If its a single event then put it inside an array.
+    if (isString(events)) {
+        events = [events];
     }
 
-
-    return {
-        /**
-         * Stores events in a map and bind one or more event listeners to an element.
-         *
-         * @param {targetElement} targetElement - Event target element.
-         * @param {String|Array} events - Event(s) type(s)
-         * @param {CallableFunction} handler - Event handler.
-         * @param {Object|Boolean} options - Event options.
-         */
-        on: (targetElement, events, handler, options) => {
-
-            if (targetElement) {
-
-                let eventData = [events, handler, options || false];
-                // Get element's events array.
-                let elementEvents = eventMap.get(targetElement);
-                // If the element has events then append the new event to the events array,
-                // otherwise create a new array containing the new event.
-                eventMap.set(targetElement, elementEvents ? elementEvents.push(eventData) && elementEvents : [eventData]);
-
-                binder(targetElement, eventData, 'addEventListener');
-            }
-        },
-
-        /**
-         * Removes all event listeners.
-         */
-        clear: () => {
-            eventMap.forEach((events, targetElement) => {
-                events.forEach(eventData => binder(targetElement, eventData, 'removeEventListener'));
-            });
-        }
-    }
+    events.forEach(event => {
+        targetElement[method](event, handler, options);
+    });
 }
+
+/**
+ * Binds events to an element and stores listener in an array.
+ *
+ * @param {Array} listeners - Event Listeners.
+ * @param {targetElement} targetElement - Event Target.
+ * @param {String|Array} events - Event type.
+ * @param {CallableFunction} handler - Event handler.
+ * @param {Object|Boolean} options - Event options.
+ * @returns {Array}
+ */
+export const bindEvent = (listeners, targetElement, events, handler, options) => {
+    options = options || false;
+    let eventData = [targetElement, events, handler, options];
+    binder(eventData);
+    listeners.push(eventData);
+
+    return listeners;
+}
+
+/**
+ * Unbinds all listeners attach to an element and removes them from listeners array.
+ *
+ * @param {Array} listeners - Event listeners.
+ * @param {targetElement} targetElement - Event Target.
+ * @returns {Array}
+ */
+export const unbindEvent = (listeners, targetElement) =>
+    // Filter out listeners attached to this TargetElement.
+    listeners.filter(eventData => 
+        targetElement === eventData[0] ? 
+            binder(eventData, true)
+            : true
+    );
