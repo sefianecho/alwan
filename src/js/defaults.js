ec8e7f9beecf1bbaeebe97c00221f4f05af13953
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-09-11 14:52:51 +0100

    add docs

--- src/js/defaults.js
+++ src/js/defaults.js
@@ -1,14 +1,81 @@
-export const defaults = {
+/**
+ * SC Picker default options.
+ */
+ export const defaults = {
+    // Set the container's (widget) id.
+    id: '',
+
+    // One or many classes separated by a white space,
+    // to add it to the preset button.
+    classname: '',
+
+    // Choose a theme, 'dark' or 'light'.
+    theme: 'light',
+
+    // Toggle picker's visibility (Show/Hide),
+    // Setting this to false keeps the picker visible.
+    toggle: true,
+
+    // Display the picker container as a pop-up (a box that floats on top of the page content),
+    // if it's false, picker container will be displayed as a block (embeded in the page's content).
+    popover: true,
+
+    // Set the position of the popper (if popover is set to true) relative to the reference element,
+    // the position has two values seperated by a dash (-),
+    // the first value is the direction (top, bottom, right, left),
+    // the second value is the alignment (start, center, end), omitting this value will default to center.
+    // e.g. 'bottom-start': 'bottom' places the picker below the reference element,
+    // and 'start' aligns the left side of the container with the left side of the reference element.
+    // Note: 
+    // If the picker container has no space to be placed, it will auto-position itself.
+    // based on the available space.
+    position: 'bottom-start',
+
+    // Set the gap (in pixels) between the picker container and the reference element.
+    margin: 8,
+
+    // Replace the reference element with a pre-styled button.
     preset: true,
+
+    // Initial color.
+    color: '#000',
+
+    // Default color.
+    default: '#000',
+
+    // Target can be a selector or an HTML element,
+    // If the option popover is true, the picker container will be positionned retalive to this element,
+    // instead of the reference element.
+    // else if popover option is false, the picker container will be appended as a child into this element.
+    target: '',
+
+    // Disable the picker, users won't be able to pick colors.
+    disabled: false,
+
+    // Initial color format.
+    format: 'rgb',
+
+    // For the formats 'hsl' and 'rgb', choose a single input to display the color string,
+    // or if false, display an input for each color channel.
+    singleInput: false,
+
+    // Choose color formats for the picker input, 'hsl', 'rgb' or 'hex',
+    // No input will be displayed if the array is empty.
     inputs: {
-        hex: true,
         rgb: true,
+        hex: true,
         hsl: true,
     },
-    singleInput: false,
-    format: 'rgb',
+
+    // Support alpha channel and display opacity slider.
     opacity: true,
+
+    // Preview the color.
     preview: true,
+
+    // Add/Remove a copy button.
     copy: true,
-    swatches: []
+
+    // Array of color strings, invalid color strings will default to rgb(0,0,0).
+    swatches: [],
 }
--- src/js/index.js
+++ src/js/index.js
@@ -11,10 +11,20 @@ import { HSVToHSL, HSVToRGB, RGBToHEX, toString } from "./lib/colors";
 import { EventListener } from "./core/events/EventListener";
 import { binder } from "./core/events/EventBinder";
 
+
 export default class Talwin {
 
+    /**
+     * Talwin defaults.
+     */
     static defaults = defaults;
 
+    /**
+     * Talwin picker constructor.
+     *
+     * @param {String|Element} reference - The reference element.
+     * @param {Object} options - Options.
+     */
     constructor(reference, options) {
         reference = getElement(reference);
         const talwin = this;
@@ -25,30 +35,66 @@ export default class Talwin {
         initialize(talwin);
     }
 
+    /**
+     * Sets new options.
+     *
+     * @param {Object} options - Talwin options.
+     */
     setOptions(options) {
         initialize(this, options);
     }
 
+    /**
+     * Gets the state of the picker whether it's opened or closed.
+     *
+     * @returns {Boolean}
+     */
     isOpen() {
         return this._ui.app.isOpen();
     }
 
+    /**
+     * Opens the picker.
+     */
     open() {
         this._ui.app.open();
     }
 
+    /**
+     * Closes the picker.
+     */
     close() {
         this._ui.app.close();
     }
 
+    /**
+     * Toggles (opens/closes) the picker.
+     */
     toggle() {
         this._ui.app.toggle();
     }
 
+    /**
+     * Attaches an event handler function for an event.
+     *
+     * @param {String} type - Event type.
+     * @param {CallableFunction} handler - Event handler.
+     */
     on(type, handler) {
         this._e.on(type, handler);
     }
 
+    /**
+     * Detaches one or more event handlers.
+     *
+     * Note:
+     * omitting handler, remove all handlers from the event,
+     * omitting both event type and handler, remove all handlers that are,
+     * attached to all events.
+     *
+     * @param {String} type - Event type.
+     * @param {CallableFunction} handler - Event handler.
+     */
     off(type, handler) {
         this._e.off(type, handler);
     }
@@ -101,58 +147,96 @@ export default class Talwin {
         return talwin;
     }
 
+    /**
+     * Gets color.
+     *
+     * @returns {Object}
+     */
     getColor() {
         return this._clr.value;
     }
 
+    /**
+     * Adds a swatch.
+     *
+     * @param {String} color - Color.
+     */
     addSwatch(color) {
         this._ui.swatches.add(color);
     }
 
+    /**
+     * Removes a swatch.
+     *
+     * @param {String|Number} swatch - Can a color string or it's index in the swatches array.
+     */
     removeSwatch(swatch) {
         this._ui.swatches.remove(swatch);
     }
 
+    /**
+     * Enables picker.
+     */
     enable() {
         this._ui.app.disable(false);
     }
 
+    /**
+     * Disables picker.
+     */
     disable() {
         this._ui.app.disable(true);
     }
 
+    /**
+     * Resets to default color.
+     */
     reset() {
         this.setColor(this.config.default);
     }
 
+    /**
+     * Repositions picker if it's displayed as a popover.
+     */
     reposition() {
         this._ui.app.reposition();
     }
 
-    trigger(event) {
+    /**
+     * Executes all handlers attached to the specified event.
+     *
+     * @param {String} type - Event type.
+     */
+    trigger(type) {
         let talwin = this;
         let emit = talwin._e.emit;
 
-        if (event === 'color' || event === 'change') {
-            emit(event, talwin._clr.value, talwin);
+        if (type === 'color' || type === 'change') {
+            emit(type, talwin._clr.value, talwin);
         } else {
-            emit(event);
+            emit(type);
         }
     }
 
+    /**
+     * Destroy picker and free up memory.
+     */
     destroy() {
 
         let talwin = this;
         let components = talwin._ui;
 
+        // Initialize the reference element back.
         components.ref.init({ preset: false, toggle: true });
 
+        // Remove all events.
         objectIterator(components, components => {
             components.e.forEach(listener => {
                 binder(listener, true);
             });
         });
 
+        // Remove all properties of this instance.
         objectIterator(talwin, (value, key) => {
             talwin[key] = null;
         });
