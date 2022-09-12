0dcf851f4c51348819f5e623fe65d3a95fa79d65
Sefiane Chouaib <sefian.chouaib@gmail.com>
2022-09-12 01:04:49 +0100

    build

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -23,10 +23,7 @@ module.exports = {
                 test: /\.m?js$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                    loader: 'babel-loader'
                 }
             }
         ]
