5f7a7b838dc2345d8a4df5291193d8e905b42faf
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-02-23 19:12:11 +0100

    add number utility

---
+++ src/utils/number.js
@@ -0,0 +1,24 @@
+export const { parseFloat: float, parseInt: int, isFinite: isNumeric } = Number;
+export const { min, max, abs, round } = Math;
+
+/**
+ * Keeps a number in a range.
+ *
+ * @param {number} number - A number to keep it between two numbers.
+ * @param {number} upperBound - Max.
+ * @param {number} lowerBound - Min.
+ * @returns {number}
+ */
+export const numberRange = (number, upperBound = 100, lowerBound = 0) => {
+    return min(max(number, lowerBound), upperBound);
+}
+
+/**
+ * Angle value in degrees, it must be between 0 and 360.
+ *
+ * @param {number} angle - Angle.
+ * @returns {Number}
+ */
+export const normalizeAngle = angle => {
+    return (round(angle) % 360 + 360) % 360;
+}
