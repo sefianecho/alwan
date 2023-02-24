b9c08e7e42346fe18277b7ece2d8a6cbab8ee8e7
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-02-24 14:53:11 +0100

    add classnames

---
+++ src/classnames.js
@@ -0,0 +1,27 @@
+import { BUTTON, INPUT, OPEN } from "./constants";
+
+const COLLAPSE = 'collapse';
+export const ALWAN_CLASSNAME = 'alwan';
+export const CONTAINER_CLASSNAME = `${ALWAN_CLASSNAME}__container`;
+export const PALETTE_CLASSNAME = `${ALWAN_CLASSNAME}__palette`;
+export const MARKER_CLASSNAME = `${ALWAN_CLASSNAME}__marker`;
+export const PREVIEW_CLASSNAME = `${ALWAN_CLASSNAME}__preview`;
+export const BUTTON_CLASSNAME = `${ALWAN_CLASSNAME}__${BUTTON}`;
+export const COPY_BUTTON_CLASSNAME = `${BUTTON_CLASSNAME} ${ALWAN_CLASSNAME}__copy-${BUTTON}`;
+export const SLIDER_CLASSNAME = `${ALWAN_CLASSNAME}__slider`;
+export const SLIDERS_CLASSNAME = `${SLIDER_CLASSNAME}s`;
+export const HUE_SLIDER_CLASSNAME = `${SLIDER_CLASSNAME} ${SLIDER_CLASSNAME}--hue`;
+export const ALPHA_SLIDER_CLASSNAME = `${SLIDER_CLASSNAME} ${SLIDER_CLASSNAME}--alpha`;
+export const INPUT_CLASSNAME = `${ALWAN_CLASSNAME}__${INPUT}`;
+export const INPUTS_CLASSNAME = `${INPUT_CLASSNAME}s`;
+export const SWATCH_CLASSNAME = `${ALWAN_CLASSNAME}__swatch`;
+export const SWATCHES_CLASSNAME = `${SWATCH_CLASSNAME}es`;
+export const PRESET_BUTTON_CLASSNAME = `${BUTTON_CLASSNAME} ${ALWAN_CLASSNAME}__preset-${BUTTON}`;
+export const OVERLAY_CLASSNAME = `${ALWAN_CLASSNAME}__overlay`;
+export const COLLAPSE_BUTTON_CLASSNAME = `${BUTTON_CLASSNAME} ${ALWAN_CLASSNAME}__${COLLAPSE}-${BUTTON}`;
+
+export const POPUP_CLASSNAME = `${ALWAN_CLASSNAME}--popup`;
+export const OPEN_CLASSNAME = `${ALWAN_CLASSNAME}--${OPEN}`;
+export const DISABLED_CLASSNAME = `${ALWAN_CLASSNAME}--disabled`;
+export const FOCUS_CLASSNAME = `${ALWAN_CLASSNAME}--focus`;
+export const COLLAPSE_CLASSNAME = `${ALWAN_CLASSNAME}--${COLLAPSE}`;
