e20987bae2708629a15a4555167f49b2b7d8b6f1
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-02-25 13:15:13 +0100

    add color functions

---
+++ src/colors/converter.js
@@ -0,0 +1,134 @@
+import { int, max, min, normalizeAngle, round } from "../utils/number";
+
+/**
+ * Helper function used for converting HSV to RGB.
+ *
+ * @param {number} k - Positive coefficient.
+ * @param {number} s - Saturation.
+ * @param {number} v - Value.
+ * @returns {number}
+ */
+const fn = (k, s, v) => {
+    return (v - v * s * max(0, min(k, 4 - k, 1))) * 255;
+}
+
+/**
+ * Converts HSV to RGB.
+ *
+ * @param {object} param0 - HSV color object.
+ * @returns {object} - RGB color object.
+ */
+export const HSVToRGB = ({ h, s, v, a }) => {
+    h /= 60;
+    return {
+        r: round(fn((5 + h) % 6, s, v)),
+        g: round(fn((3 + h) % 6, s, v)),
+        b: round(fn((1 + h) % 6, s, v)),
+        a
+    }
+}
+
+
+/**
+ * Converts hex color string to RGB object.
+ *
+ * @param {string} hex - Hexadecimal color.
+ * @returns {object} - RGB color object.
+ */
+export const HEXToRGB = hex => {
+    return {
+        r: int(hex.slice(1, 3), 16),
+        g: int(hex.slice(3, 5), 16),
+        b: int(hex.slice(5, 7), 16),
+        a: 1
+    }
+}
+
+/**
+ * Converts a decimal number to hexadecimal.
+ *
+ * @param {number} number - A decimal number.
+ * @returns {string} - Hexadecimal.
+ */
+const toHex = number => {
+    return (number < 16 ? '0' : '') + number.toString(16);
+}
+
+
+/**
+ * Converts RGB object to Hex string.
+ *
+ * @param {object} param0 - RGB color object.
+ * @returns {string} - Hex color.
+ */
+export const RGBToHEX = ({ r, g, b, a }) => {
+    return '#' + toHex(r) + toHex(g) + toHex(b) + (a < 1 ? toHex(round(a * 255)) : '');
+}
+
+
+/**
+ * Converts HSV to HSL.
+ *
+ * @param {object} param0 - HSV color object.
+ * @returns {object} - HSL object.
+ */
+export const HSVToHSL = ({ h, s, v, a }) => {
+    let l = v * (1 - s / 2);
+    return {
+        h,
+        s: round((! l || l === 1 ? 0 : (v - l) / min(l, 1 - l)) * 100),
+        l: round(l * 100),
+        a: round(a * 100) / 100
+    }
+}
+
+
+/**
+ * Converts HSL to HSV.
+ *
+ * @param {Object} hsl - HSL color object.
+ * @returns {Object}
+ */
+export const HSLToHSV = ({ h, s, l, a }) => {
+    s /= 100;
+    l /= 100;
+
+    let v = l + s * min(l, 1 - l);
+
+    return {
+        h,
+        s: v ? 2 * (1 - l / v) : 0,
+        v,
+        a
+    }
+}
+
+
+/**
+ * Converts RGB to HSV.
+ *
+ * @param {Object} rgb - RGB color object.
+ * @returns {Object}
+ */
+export const RGBToHSV = ({ r, g, b, a }) => {
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    let cMax = max(r, g, b);
+    let cMin = min(r, g, b);
+    let range = cMax - cMin;
+    let h = range === 0 ? 0
+            : cMax === r ? ((g - b) / range) % 6
+            : cMax === g ? ((b - r) / range) + 2
+            : cMax === b ? ((r - g) / range) + 4
+            : 0;
+
+    return {
+        h: normalizeAngle(h * 60),
+        s: cMax ? range / cMax : 0,
+        v: cMax,
+        a: round(a * 100) / 100
+    }
+}
---
+++ src/colors/stringify.js
@@ -0,0 +1,25 @@
+import { HSL_FORMAT } from "../constants";
+
+/**
+ * Converts RGB or HSL color objects to string.
+ *
+ * @param {object} color - HSL or RGB color object.
+ * @param {string} format - hsl or rgb.
+ * @returns {string} rgb or hsl string.
+ */
+export const stringify = (color, format) => {
+    let percentage = '';
+    let opacity = '';
+    let a = color.a;
+
+    if (format === HSL_FORMAT) {
+        percentage = '%';
+    }
+
+    if (a < 1) {
+        format += 'a';
+        opacity = ', ' + a;
+    }
+
+    return `${format}(${color[format[0]]}, ${color[format[1]] + percentage}, ${color[format[2]] + percentage + opacity})`;
+}
