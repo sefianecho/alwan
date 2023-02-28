d493dbcc94224f1618654041edd4e7f54c546e42
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-02-28 17:25:35 +0100

    refactor color state

---
+++ src/core/color.js
@@ -0,0 +1,235 @@
+import { HSLToHSV, HSVToHSL, HSVToRGB, RGBToHEX, RGBToHSV } from "../colors/converter";
+import { parseColor } from "../colors/parser";
+import { stringify } from "../colors/stringify";
+import { COLOR_PROPERTY, HEX_FORMAT, HSL_FORMAT, HSV_FORMAT, RGB_FORMAT } from "../constants";
+import { setCustomProperty } from "../utils/dom";
+import { isEqual, merge } from "../utils/object";
+
+/**
+ * Creates the core color state and UI updater.
+ *
+ * @param {object} alwan - Alwan instance.
+ * @returns {object} Core color state.
+ */
+export const color = (alwan) => {
+    /**
+     * HSV color object.
+     */
+    let HSV = {
+        h: 0,
+        s: 0,
+        v: 0,
+        a: 1
+    };
+
+    /**
+     * RGB color object.
+     *
+     * @type {object}
+     */
+    let RGB;
+
+    /**
+     * HSL color object.
+     *
+     * @type {object}
+     */
+    let HSL;
+
+    /**
+     * RGB color string.
+     *
+     * @type {string}
+     */
+    let rgbString;
+
+    /**
+     * HSL color string.
+     *
+     * @type {string}
+     */
+    let hslString;
+
+    /**
+     * HEX color string.
+     *
+     * @type {string}
+     */
+    let hex;
+
+    /**
+     * Alwan options.
+     */
+    let config = alwan.config;
+
+    /**
+     * Gets color data.
+     *
+     * @param {object} color - Color object.
+     * @param {string} format - Color format.
+     * @param {boolean} asArray - Gets data as an array.
+     * @param {string} str - Color string.
+     * @returns {object} - Color data.
+     */
+    let colorData = (color, format, asArray, str) => {
+        return merge(
+            (format + (config.opacity ? 'a' : '')).split('').reduce((output, channel, index) => {
+                output[asArray ? index : channel] = color[channel];
+                return output;
+            }, asArray ? [] : {}),
+
+            format !== HSV_FORMAT ? { toString: () => str } : {}
+        )
+    }
+
+    /**
+     * Value methods.
+     */
+    let value = {
+        hsv: asArray => colorData(HSV, HSV_FORMAT, asArray),
+        rgb: asArray => colorData(RGB, RGB_FORMAT, asArray, rgbString),
+        hsl: asArray => colorData(HSL, HSL_FORMAT, asArray, hslString),
+        hex: () => hex,
+    };
+
+
+    /**
+     * Color api.
+     */
+    let self = {
+        /**
+         * Updates core color, css custom properties and inputs values.
+         *
+         * @param {object} hsv - HSV color object.
+         * @param {object|undefined} rgb - RGB color object.
+         * @param {boolean} isInputs - If true don't set inputs values.
+         */
+        _update(hsv, rgb, isInputs) {
+    
+            if (! config.disabled) {
+                let { _palette, _sliders, _inputs, _utility } = alwan._components;
+    
+                hsv = hsv || HSV;
+
+                if (hsv !== HSV) {
+                    merge(HSV, hsv);
+
+                    RGB = rgb || HSVToRGB(HSV);
+                    rgbString = stringify(RGB, RGB_FORMAT);
+                    hex = RGBToHEX(RGB);
+                    HSL = HSVToHSL(HSV);
+                    hslString = stringify(HSL, HSL_FORMAT);
+                }
+
+                // Update UI.
+                setCustomProperty(alwan._reference._element, COLOR_PROPERTY, rgbString);
+                setCustomProperty(_palette._element, 'hue', hsv.h);
+                // Preview the current color.
+                _utility._preview(rgbString);
+                _sliders._sliderGradient(RGB);
+
+                if (! isInputs) {
+                    _inputs._setValue(self._getColorByFormat(config.singleInput));
+                }
+            }
+        },
+
+        /**
+         * Same as update method plus it updates palette's marker position,
+         * and sliders values.
+         *
+         * @param {object} hsv - HSV color object.
+         * @param {object|undefined} rgb - RGB color object.
+         * @param {boolean} isInputs - If true don't set inputs values.
+         */
+        _updateAll(hsv, rgb, isInputs) {
+            let { _palette, _sliders } = alwan._components;
+            self._update(hsv, rgb, isInputs);
+            _palette._updateMarker(HSV);
+            _sliders._setValue(HSV);
+        },
+
+        /**
+         * Gets a color string or object by a format.
+         *
+         * @param {boolean} asString - Whether to get the color as a string.
+         * @param {string} format - Color format.
+         * @returns {object|string} - Color.
+         */
+        _getColorByFormat: (asString, format = config.format) => {
+    
+            if (format === RGB_FORMAT) {
+                if (asString) {
+                    return rgbString;
+                }
+                return RGB;
+            }
+    
+            if (format === HSL_FORMAT) {
+                if (asString) {
+                    return hslString;
+                }
+                return HSL;
+            }
+    
+            if (format === HEX_FORMAT) {
+                return hex;
+            }
+    
+            return '';
+        },
+
+        /**
+         * Sets color.
+         *
+         * @param {string|object} color - Color value.
+         * @param {boolean} isInputs - Is inputs component.
+         * @returns {boolean} - Whether the color state is changed or not.
+         */
+        _set(color, isInputs = false) {
+            let { _format, _color } = parseColor(color);
+            let isChanged = ! isEqual(_color, self._getColorByFormat(false, _format));
+            let isRGB = _format === RGB_FORMAT;
+    
+            if (isChanged) {
+                self._updateAll(
+                    // Convert colors (RGB or HSL) to HSV.
+                    isRGB ? RGBToHSV(_color) : HSLToHSV(_color),
+                    // If the parsed color is RGB.
+                    isRGB ? _color : false,
+                    isInputs
+                );
+                
+            }
+    
+            return isChanged;
+        },
+
+        /**
+         * Creates and gets event object.
+         *
+         * @param {string} type - Event type.
+         * @param {Element} src - Source element.
+         * @returns {object} - Event object.
+         */
+        _event: (type, src) => {
+            return merge(
+                {
+                    type,
+                    src,
+                    value: rgbString
+                },
+                value
+            )
+        },
+
+        /**
+         * Gets color value.
+         *
+         * @returns {object} - Color value.
+         */
+        _value: () => merge({ value: rgbString }, value),
+    }
+
+    return self;
+}
