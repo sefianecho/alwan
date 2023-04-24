76608875d6c961ead684a5bd7f24b4127311af3f
Sefiane Chouaib <sefiane.chouaib@outlook.com>
2023-04-24 19:14:44 +0100

    add rgb to hex

--- src/colors/stringify.js
+++ src/colors/stringify.js
@@ -1,19 +1,30 @@
-import { HSL_FORMAT } from "../constants/globals";
+import { HEX_FORMAT, HSL_FORMAT } from "../constants/globals";
+import { round } from "../utils/number.js";
+
+/**
+ * Converts a decimal number to hexadecimal.
+ *
+ * @param {number} number - A decimal number.
+ * @returns {string} - Hexadecimal.
+ */
+const toHex = number => {
+    return (number < 16 ? '0' : '') + number.toString(16);
+}
 
 /**
  * Converts RGB or HSL color objects to string.
  *
  * @param {object} color - HSL or RGB color object.
- * @param {string} format - hsl or rgb.
+ * @param {string} format - Color format (hex, rgb or hsl).
+ * @param {boolean} opaque - If true, output color string with no opacity.
  * @returns {string} rgb or hsl string.
  */
 export const stringify = (color, format, opaque) => {
-    let percentage = '';
     let opacity = '';
     let a = color.a;
 
-    if (format === HSL_FORMAT) {
-        percentage = '%';
+    if (format === HEX_FORMAT) {
+        return '#' + toHex(color.r) + toHex(color.g) + toHex(color.b) + (a < 1 ? toHex(round(a * 255)) : '');
     }
 
     if (a < 1 && ! opaque) {
@@ -21,5 +32,9 @@ export const stringify = (color, format, opaque) => {
         opacity = ', ' + a;
     }
 
-    return `${format}(${color[format[0]]}, ${color[format[1]] + percentage}, ${color[format[2]] + percentage + opacity})`;
+    if (format === HSL_FORMAT) {
+        return `${format}(${color.h}, ${color.s}%, ${color.l}%${opacity})`;
+    }
+
+    return `${format}(${color.r}, ${color.g}, ${color.b + opacity})`;
 }
