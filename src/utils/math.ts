9456f3f4da09124236342790ebf9ebd1dbfee462
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-10-21 21:10:18 +0100

    refactor number to math ts

---
+++ src/utils/math.ts
@@ -0,0 +1,24 @@
+export const { parseFloat: float, parseInt: int, isFinite: isNumeric } = Number;
+export const { min, max, abs, round, PI } = Math;
+
+/**
+ * Keeps a number in a range.
+ *
+ * @param number - A number to keep it between two numbers.
+ * @param upperBound - Max.
+ * @param lowerBound - Min.
+ * @returns - Bound number.
+ */
+export const boundNumber = (number: number, upperBound = 100, lowerBound = 0) =>
+    number > upperBound ? upperBound : number < lowerBound ? lowerBound : number;
+
+/**
+ * Angle value in degrees, it must be between 0 and 360.
+ *
+ * @param angle - Angle.
+ * @returns - Normalized angle.
+ */
+export const normalizeAngle = (angle: number) => {
+    angle %= 360;
+    return round(angle < 0 ? angle + 360 : angle);
+};
