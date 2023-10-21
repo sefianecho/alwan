c566a8b75a040bd73dbab3a8777cc503a6e5283c
Sefiane Chouaib <sefian.chouaib@gmail.com>
2023-10-21 20:53:31 +0100

    refactor: alwan defaults

---
+++ src/constants/defaults.ts
@@ -0,0 +1,37 @@
+import type { alwanConfig } from '../types';
+
+export const alwanDefaults: alwanConfig = {
+    id: '',
+    classname: '',
+    theme: 'light',
+    toggle: true,
+    popover: true,
+    position: 'bottom-start',
+    margin: 0,
+    preset: true,
+    color: '#000',
+    default: '#000',
+    target: '',
+    disabled: false,
+    format: 'rgb',
+    singleInput: false,
+    inputs: true,
+    opacity: true,
+    preview: true,
+    copy: true,
+    swatches: [],
+    toggleSwatches: false,
+    closeOnScroll: false,
+    i18n: {
+        palette: 'Color picker',
+        buttons: {
+            copy: 'Copy color to clipboard',
+            changeFormat: 'Change color format',
+            swatch: 'Color swatch',
+        },
+        sliders: {
+            hue: 'Change hue',
+            alpha: 'Change opacity',
+        },
+    },
+};
